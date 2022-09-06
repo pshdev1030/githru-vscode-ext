3ff4c60fcfa6c79648d4af1f34ced001d1744ac2
pshdev1030 <pshdev1030@gmail.com>
2022-09-07 00:17:22 +0900

    feat: temporalfilter util 함수 추가
    날짜별 commit, cloc를 가져오고 오름차순으로 정렬하였습니다.

--- packages/view/src/components/TemporalFilter/TemporalFilter.tsx
+++ packages/view/src/components/TemporalFilter/TemporalFilter.tsx
@@ -1,10 +1,62 @@
-import type { GlobalProps } from "types";
+import { select } from "d3";
+import dayjs from "dayjs";
+import { useEffect, useRef } from "react";
+
+import type { ClusterNode } from "types";
+import type { GlobalProps } from "types/global";
+
+const filterData = (data: ClusterNode[]): any => {
+  const dateMap = new Map();
+
+  data.forEach((cluster) => {
+    const commitLength = cluster.commitNodeList.length;
+
+    cluster.commitNodeList
+      .filter((commitNode) => commitNode.nodeTypeName === "COMMIT")
+      .forEach((commitNode) => {
+        // 날짜 계산
+        const { commitDate } = commitNode.commit;
+        const formattedCommitDate = dayjs(commitDate).format("YYYY MM DD");
+
+        // cloc 계산
+        const { insertions, deletions } = commitNode.commit.diffStatistics;
+        const cloc = insertions + deletions;
+
+        // mapItem 만들기
+        const mapItem = dateMap.get(formattedCommitDate) || {};
+
+        mapItem.cloc = mapItem.cloc + cloc || cloc;
+        mapItem.commit = mapItem.commit + commitLength || commitLength;
+
+        dateMap.set(formattedCommitDate, mapItem);
+      });
+  });
+
+  return Array.from(dateMap.entries()).sort((a, b) => dayjs(a[0]).diff(b[0]));
+};
 
 const TemporalFilter = ({ data }: GlobalProps) => {
-  if (!data) {
-    console.log(data);
-  }
-  return <div className="temporal-filter">TemporalFilter</div>;
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const svgRef = useRef<SVGSVGElement>(null);
+
+  useEffect(() => {
+    if (!wrapperRef.current) return;
+
+    console.log(filterData(data));
+
+    const svg = select(svgRef.current);
+
+    const { width, height } =
+      wrapperRef.current && wrapperRef.current.getBoundingClientRect();
+
+    svg.attr("width", width).attr("height", height);
+  }, [data]);
+
+  return (
+    <div ref={wrapperRef}>
+      <svg ref={svgRef} />
+    </div>
+  );
 };
 
 export default TemporalFilter;
